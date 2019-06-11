855fcdb586670cab14a6d5961cfbea1b00ed72c4
Leandro Amaro Garcia <>
2019-06-11 18:54:55 -0300

    Modificación para Heroku (installed serve)

---
+++ build/precache-manifest.1d106d173b4d425ea22f58817b45ac90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a103ab05bd909f9e035e10c71ff223e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64e0d57e813f8665f7c1",
+    "url": "/static/css/main.0bee7b91.chunk.css"
+  },
+  {
+    "revision": "1524ad07b78f3cb33a70",
+    "url": "/static/js/2.3437c2a1.chunk.js"
+  },
+  {
+    "revision": "64e0d57e813f8665f7c1",
+    "url": "/static/js/main.cf9db425.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3763bced17b053f0f4d25a8be6945f41",
+    "url": "/static/media/logoNews.3763bced.png"
+  }
+]);
